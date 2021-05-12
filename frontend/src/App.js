b0a3e1e752099ce66780f0ea3b636601a8d4973b
jazmine2411 <mzaiem27@gmail.com>
2021-05-12 23:17:57 +0100

    share code on github

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,31 +1,175 @@
 import React from "react";
-//import { BrowserRouter as Router, Route } from 'react-router-dom';
 import "./index.css";
-//import HomeScreen from './screens/HomeScreen'
-//import ProductScreen from './screens/ProductScreen'
+import { BrowserRouter as Router , Route, Link } from 'react-router-dom';
+import { useDispatch, useSelector } from 'react-redux';
 
+import { signout } from './actions/userActions';
+
+import PrivateRoute from './components/PrivateRoute';
+
+import HomeScreen from './screens/HomeScreen';
+import ProductScreen from './screens/ProductScreen';
+import CartScreen from "./screens/CartScreen";
+import SigninScreen from './screens/SigninScreen';
+import RegisterScreen from './screens/RegisterScreen';
+import ProfileScreen from './screens/ProfileScreen';
+import ShippingAddressScreen from './screens/ShippingAddressScreen';
+import PaymentMethodScreen from './screens/PaymentMethodScreen';
+import PlaceOrderScreen from "./screens/PlaceOrderScreen";
+import OrderScreen from './screens/OrderScreen';
+import OrderHistoryScreen from './screens/OrderHistoryScreen';
+import AdminRoute from './components/AdminRoute';
+import ProductListScreen from './screens/ProductListScreen';
+import ProductEditScreen from './screens/ProductEditScreen';
+import OrderListScreen from './screens/OrderListScreen';
+import UserListScreen from './screens/UserListScreen';
+import UserEditScreen from './screens/UserEditScreen';
+import SearchBox from './components/SearchBox';
+import SearchScreen from './screens/SearchScreen';
+import SellerRoute from './components/SellerRoute';
+import SellerScreen from './screens/SellerScreen';
 function App() {
+  const cart = useSelector(state => state.cart);
+  const { cartItems } = cart;
+  const userSignin = useSelector((state) => state.userSignin);
+  const { userInfo } = userSignin;
+  const dispatch = useDispatch();
+
+  const signoutHandler = () => {
+    dispatch(signout());
+  };
   return (
-    
-    <div className="grid-container">
-      <header className="row">
-        <div>
-          <a className="brand" href="/">
-            Jassmine
-          </a>
-        </div>
-        <div>
-          <a href="/cart">Cart</a>
-          <a href="/signin">Sign In</a>
-        </div>
-      </header>
-      <main>
-    
-        
-      </main>
-      <footer className="row center">All Right Reserved</footer>
-    </div>
-   
+    <Router>
+      <div className="grid-container">
+        <header className="row">
+          <div>
+            <Link to="/" className="brand" >
+              JassmineShop
+            </Link>
+          </div>
+          <div>
+            <Route
+              render={({ history }) => (
+                <SearchBox history={history}></SearchBox>
+              )}
+            ></Route>
+          </div>
+          <div>
+            <Link to="/cart">Cart
+              { cartItems.length >0 && (
+                <span className='badge'>{ cartItems.length }</span>
+              )}
+            </Link>
+           
+            {userInfo ? (
+              <div className="dropdown">
+                <Link to="#">
+                  {userInfo.name} <i className="fa fa-caret-down"></i>{' '}
+                </Link>
+                <ul className="dropdown-content">
+                <li>
+                    <Link to="/orderhistory">Order History</Link>
+                  </li>
+                  <Route
+                    path="/search/name/:name?"
+                    component={SearchScreen}
+                    exact
+                  ></Route>
+                  <li>
+                    <Link to="#signout" onClick={signoutHandler}>
+                      Sign Out
+                    </Link>
+                  </li>
+                  <li>
+                    <Link to="/profile">User Profile</Link>
+                  </li>
+                </ul>
+              </div>
+            ) : (
+              <Link to="/signin">Sign In</Link>
+            )}
+
+            {userInfo && userInfo.isSeller && (
+              <div className="dropdown">
+                <Link to="#admin">
+                  Seller <i className="fa fa-caret-down"></i>
+                </Link>
+                <ul className="dropdown-content">
+                  <li>
+                    <Link to="/productlist/seller">Products</Link>
+                  </li>
+                  <li>
+                    <Link to="/orderlist/seller">Orders</Link>
+                  </li>
+                </ul>
+              </div>
+            )}
+
+            {userInfo && userInfo.isAdmin && (
+              <div className="dropdown">
+                <Link to="#admin">
+                  Admin <i className="fa fa-caret-down"></i>
+                </Link>
+                <ul className="dropdown-content">
+                  <li>
+                    <Link to="/dashboard">Dashboard</Link>
+                  </li>
+                  <li>
+                    <Link to="/productlist">Products</Link>
+                  </li>
+                  <li>
+                    <Link to="/orderlist">Orders</Link>
+                  </li>
+                  <li>
+                    <Link to="/userlist">Users</Link>
+                  </li>
+                </ul>
+              </div>
+            )}
+          </div>
+        </header>
+        <main>
+            <Route path="/seller/:id" component={SellerScreen}></Route>
+            <Route path='/cart/:id?' component = { CartScreen } />
+            <Route path="/product/:id" component={ProductScreen} exact ></Route>
+            <Route path="/product/:id/edit" component={ProductEditScreen} exact ></Route>
+            <Route path="/signin" component={SigninScreen}></Route>
+            <Route path="/register" component={RegisterScreen}></Route>
+            <PrivateRoute path="/profile" component={ProfileScreen}></PrivateRoute>
+            <AdminRoute path="/productlist" component={ProductListScreen} exact ></AdminRoute>
+            <AdminRoute
+            path="/orderlist"
+            component={OrderListScreen} exact
+            ></AdminRoute>
+            <AdminRoute path="/userlist" component={UserListScreen}></AdminRoute>
+            <AdminRoute
+            path="/user/:id/edit"
+            component={UserEditScreen}
+          ></AdminRoute>
+          <SellerRoute
+            path="/productlist/seller"
+            component={ProductListScreen}
+          ></SellerRoute>
+          <SellerRoute
+            path="/orderlist/seller"
+            component={OrderListScreen}
+          ></SellerRoute>
+            <Route path="/shipping" component={ShippingAddressScreen}></Route>
+            <Route path="/payment" component={PaymentMethodScreen}></Route>
+            <Route path="/placeorder" component={PlaceOrderScreen}></Route>
+            <Route path="/order/:id" component={OrderScreen}></Route>
+            <Route path="/orderhistory" component={OrderHistoryScreen}></Route>
+            <Route
+            path="/search/name/:name?"
+            component={SearchScreen}
+            exact
+          ></Route>
+            <Route exact path='/' component = {HomeScreen} />
+            
+        </main>
+        <footer className="row center">All right reserved</footer>
+      </div>
+    </Router>
   );
 }
 
---
+++ frontend/src/screens/ProductScreen.js
@@ -0,0 +1,131 @@
+import React, { useEffect, useState } from 'react'
+import { useDispatch, useSelector } from 'react-redux';
+import { Link } from 'react-router-dom'
+import { detailsProduct } from '../actions/productActions';
+import LoadingBox from '../components/LoadingBox';
+import Rating from "../components/Rating";
+import MessageBox from './../components/MessageBox';
+
+const ProductScreen = (props) => {
+
+    const dispatch = useDispatch();
+    const productId = props.match.params.id;
+    const [qty, setQty] = useState(1);
+    const productDetails = useSelector((state) => state.productDetails);
+    const { loading, error, product } = productDetails;
+
+    useEffect(() => {
+       dispatch(detailsProduct(productId));
+    }, [dispatch,productId]);
+   
+    const addToCartHandler = () => {
+      props.history.push(`/cart/${productId}?qty=${qty}`);
+    };
+    return (
+      <div>
+      {loading ? (
+        <LoadingBox></LoadingBox>
+      ) : error ? (
+        <MessageBox variant='danger'>{error}</MessageBox>
+      ) : (
+        <div>
+    <Link to='/'>Back to result</Link>
+      <div className="row top">
+        <div className="col-2">
+          <img className="medium" src={product.image} alt={product.name} />
+        </div>
+        <div className="col-1">
+          <ul>
+            <li>
+              <h1>{product.name}</h1>
+            </li>
+            <li>
+              <Rating rating={product.rating} numReviews={product.numReviews} />
+            </li>
+            <li>Price: ${product.price}</li>
+            <li>
+              Description:
+              <p>{product.description}</p>
+            </li>
+          </ul>
+        </div>
+        <div className="col-1">
+          <div className="card card-body">
+            <ul>
+            <li>
+                    Seller{' '}
+                    <h2>
+                      <Link to={`/seller/${product.seller._id}`}>
+                        {product.seller.seller.name}
+                      </Link>
+                    </h2>
+                    <Rating
+                      rating={product.seller.seller.rating}
+                      numReviews={product.seller.seller.numReviews}
+                    ></Rating>
+                  </li>
+              <li>
+                <div className="row">
+                  <div className='carditem'>Price</div>
+                  <div className="price">${product.price}</div>
+                </div>
+              </li>
+              <li>
+                <div className="row">
+                  <div className='carditem'>Status</div>
+                  <div>
+                    {product.countInStock > 0 ? (
+                      <span className="success"> In Stock</span>
+                    ) : (
+                      <span className="danger"> Unvailable</span>
+                    )}
+                  </div>
+                </div>
+              </li>
+              {
+                product.countInStock > 0 && (
+                  <>
+                  <li>
+                    <div className='row'>
+                        <div>QTY</div>
+                        <div>
+                        <select
+                              value={qty}
+                              onChange={(e) => setQty(e.target.value)}
+                            >
+                              {[...Array(product.countInStock).keys()].map(
+                                (x) => (
+                                  <option key={x + 1} value={x + 1}>
+                                    {x + 1}
+                                  </option>
+                                )
+                              )}
+                            </select>
+                        </div>
+                    </div>
+                  </li>
+                  <li>
+                    <button 
+                      className='primary block'
+                      onClick= { addToCartHandler }
+                    >
+                      Add to Cart
+                    </button>
+                  </li>
+                  </>
+                  
+                )
+              }
+              
+            </ul>
+          </div>
+        </div>
+      </div>
+    </div>
+      )}
+    </div>
+        
+    )
+}
+
+export default ProductScreen
